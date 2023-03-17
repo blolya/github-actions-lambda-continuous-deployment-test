6fd4763f9ed383b07efa26b85f6635830dcdbcc5
blolya <mast-94@mail.ru>
2023-03-17 23:18:04 +0300

    update function code

--- function.mjs
+++ function.mjs
@@ -2,7 +2,7 @@ export const handler = async(event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello, github'),
+        body: JSON.stringify('Hello, github. Deploy'),
     };
     return response;
 };
